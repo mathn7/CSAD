1da06f0be3d432401f6a57fcb5d32f78827f784d
relmo <rachid.elmontassir@etu.enseeiht.fr>
2020-07-08 18:27:45 +0200

    add docs/src

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(
     authors = "Saloua Naama, Mohamed El Waghf et Rachid ELMontassir",
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     pages = [
-
+		"Accueil" => "index.md"
             ]
     )
 
